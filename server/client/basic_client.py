883d54d0ca0931742b68a2fb6d9fc7dc66f80bf8
qiyubing <1248288510@qq.com>
2020-03-26 11:55:25 +0800

    新增查看歌单

--- server/client/basic_client.py
+++ server/client/basic_client.py
@@ -2,7 +2,7 @@
 @author: qiyubing
 @file: basic_client.py
 @time: 2020/03/23
-@description: 
+@description: 客户端基类
 """
 import abc
 from typing import List
@@ -10,10 +10,17 @@ from common.model import SearchForm, Song
 
 
 class BaseClient:
+
+    # 搜索歌曲
     @abc.abstractmethod
-    def search(self, search_form: SearchForm) -> List[Song]:
+    def search_song(self, search_form: SearchForm) -> List[Song]:
         pass
 
+    # 获取歌单
+    def get_song_list(self, search_form: SearchForm) -> List[Song]:
+        pass
+
+    # 下载歌曲
     @abc.abstractmethod
-    def download(self, song: Song, save_path: str):
+    def download(self, song_id_list: List[str], save_path: str):
         pass
